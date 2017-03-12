ea4f59fd590f22c39d0cd598018e4bd34079f5c4
Colton <coltonTB@me.com>
2017-03-12 18:56:43 -0400

    fix rendering discrepency by hacking createHref

--- src/server/lib/isomorphic-renderer.js
+++ src/server/lib/isomorphic-renderer.js
@@ -4,21 +4,38 @@ import { routes } from '../../routes';
 import { match, RouterContext } from 'react-router';
 import { renderToString, renderToStaticMarkup } from 'react-dom/server';
 
+const renderPage = (res, renderProps) => {
+  // Hijack createHref function to account for assetUrl
+  // modifying anchor tags
+  const oldCreateHref = renderProps.router.createHref;
+  renderProps.router.createHref = path =>
+    res.locals.assetUrl(oldCreateHref(path))
+
+  const appContent = renderToString(
+    <RouterContext {...renderProps}/>
+  );
+  const html = renderToStaticMarkup(
+    <Scaffold appContent={appContent} locals={res.locals}/>
+  );
+
+  res
+    .status(200)
+    .send(html);
+}
+
 const isomorphicRenderer = (req, res) => {
   // Note that req.url here should be the full URL path from
   // the original request, including the query string.
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
-      res.status(500).send(error.message);
+      res
+        .status(500)
+        .send(error.message);
     } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      res
+        .redirect(302, redirectLocation.pathname + redirectLocation.search);
     } else if (renderProps) {
-      const appContent = renderToString(
-        <RouterContext {...renderProps}/>
-      );
-      res.status(200).send(renderToStaticMarkup(
-        <Scaffold appContent={appContent} locals={res.locals}/>
-      ));
+      renderPage(res, renderProps);
     } else {
       res.status(404).send('Not found');
     }
