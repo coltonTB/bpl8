8ed1ac504e07b81158cb61f777de0865cd3ef9be
Colton <coltonTB@me.com>
2017-05-03 23:56:59 -0400

    add animations

--- src/shared/components/hero-scroll.js
+++ src/shared/components/hero-scroll.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const spinUp = keyframes`
+  0% { transform: translateY(1000px); opacity: 0; }
+  5%, 95% { transform: translateY(0px); opacity: 1; }
+  100% { transform: translateY(-1000px); opacity: 0; }
+`;
+const spinDown = keyframes`
+  0% { transform: translateY(-1000px); opacity: 0; }
+  5%, 95% { transform: translateY(0px); opacity: 1; }
+  100% { transform: translateY(1000px); opacity: 0; }
+`;
 
 const HeroScrollStyle = styled.div`
   position: absolute;
@@ -10,26 +21,59 @@ const HeroScrollStyle = styled.div`
   img {
     height: 100%;
     margin: 0 20px;
+
+    transform: translateY( ${ p => p.direction === 'up' ? '-' : ''}1000px );
+    animation-name: ${ p => p.direction === 'up' ? spinUp : spinDown };
+    animation-duration: 6s;
+    animation-timing-function: ease;
+    animation-delay: 0;
+    animation-iteration-count: infinite;
+
   }
 `;
 
-export const HeroScroll = (props, {localContext}) => {
+export const HeroScroll = React.createClass({
 
-  function getImage() {
-    const side = props.left ? 'left' : 'right';
+  contextTypes: {
+    localContext: React.PropTypes.object
+  },
+
+  animationDelay: 6,
+
+  getInitialState() {
+    const index = this.props.left ? 2 : 3;
+    return {
+      imgUrl: this.getImage(index)
+    }
+  },
+
+  componentDidMount() {
+    this.interval = window.setInterval(() => {
+      this.setState({
+        imageUrl: this.getImage()
+      });
+    }, this.animationDelay * 1000);
+  },
+
+  // componentWillUnmount() {
+  //   this.interval && window.clearInterval(this.interval);
+  // },
+
+  getImage(defaultIndex) {
+    const side = this.props.left ? 'left' : 'right';
     const base = '/images/machines';
     const images = ['01', '02', '03', '04'];
-    const index = Math.floor(Math.random() * 4);
+    const index = defaultIndex || Math.floor(Math.random() * 4);
     return `${base}/${images[index]}_${side}.png`
+  },
+
+  render() {
+    const localContext = this.context.localContext;
+    return (
+      <HeroScrollStyle direction={ this.props.left ? 'up' : 'down' }>
+        <img src={ localContext.assetUrl(this.state.imageUrl) } />
+      </HeroScrollStyle>
+    );
   }
 
-  return (
-    <HeroScrollStyle>
-      <img src={localContext.assetUrl(getImage())} />
-    </HeroScrollStyle>
-  );
-};
-
-HeroScroll.contextTypes = {
-  localContext: React.PropTypes.object
-};
+});
